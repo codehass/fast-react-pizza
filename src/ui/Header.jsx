0985d6edbbd23b122e9dd7e8bede55937254b4c2
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-10-29 15:33:33 +0100

    Fetching orders

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
 	return (
 		<header>
 			<Link to="/">Face React Pizza co.</Link>
+			<SearchOrder />
+
 			<p>Hassan</p>
 		</header>
 	);
