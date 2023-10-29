f9bf364b8ca6f8eff0e3e8879cb7700a8dde62c2
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-10-29 13:40:45 +0100

    Building app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+	return (
+		<header>
+			<Link to="/">Face React Pizza co.</Link>
+			<p>Hassan</p>
+		</header>
+	);
+}
+
+export default Header;
