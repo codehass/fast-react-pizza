7c17033169832cf4a885564747da525cbc09ce2c
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-10-30 15:56:05 +0100

    Responsive design

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase">
+    <header className="border-b border-stone-500 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Face React Pizza co.
       </Link>
