d920bb5d62c6844f1cd8b4d859987f3fa9b559bb
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-10-30 14:19:57 +0100

    Box model

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,26 +1,25 @@
-import { Outlet, useNavigation } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
-import Header from "./Header";
-import Loader from "./Loader";
+import { Outlet, useNavigation } from 'react-router-dom';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+import Loader from './Loader';
 
 function AppLayout() {
-	const navigation = useNavigation();
-	const isLoading = navigation === "loading";
+  const navigation = useNavigation();
+  const isLoading = navigation === 'loading';
 
-	return (
-		<div className="layout">
-			{isLoading && <Loader />}
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
 
-			<Header />
+      <Header />
 
-			<main>
-				<h1>Content</h1>
-				<Outlet />
-			</main>
+      <main>
+        <Outlet />
+      </main>
 
-			<CartOverview />
-		</div>
-	);
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
