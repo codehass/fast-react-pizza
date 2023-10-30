c0652504cbc50f4b4ad838fc09eeba8042331f28
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-10-30 16:04:38 +0100

    use flex

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Hassan</div>;
+  return <div className="hidden text-sm font-semibold md:block">Hassan</div>;
 }
 
 export default Username;
