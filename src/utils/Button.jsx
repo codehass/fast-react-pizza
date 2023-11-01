84d1ba63c1c7ed4db7aa2b45b3cfe3afb53044c9
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-11-01 21:40:54 +0100

    Styling the cart

--- src/utils/Button.jsx
+++ src/utils/Button.jsx
@@ -7,6 +7,8 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + "md:px-6 md:py-4 px-4 py-3",
     small: base + "px-4 py-2 md:px-5 md:py-2.5 text-sm",
+    secondary:
+      "focus:ring-STONE-200 inline-block rounded-full border-2 border-stone-300 px-4 py-2.5 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300  focus:text-stone-800 focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-3.5",
   };
 
   if (to)
