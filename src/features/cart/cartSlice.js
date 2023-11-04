0d93f8398e749dcf299784aaee9b74213f6aa4bc
Hassan EL OUARDY <hassan.elouardy06@gmail.com>
2023-11-04 11:22:41 +0100

    delete cart item

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -60,4 +60,7 @@ export const getTotalCartQuantity = (state) =>
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
 // if it's a big app this getFunction maybe produce performance issue, but we can use "reselct" library
